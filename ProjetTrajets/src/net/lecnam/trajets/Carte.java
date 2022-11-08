25137497d7c1ba6254524f5359986a0551a04998
Christophe LEDUC <christophe.leduc@etudiant.univ-reims.fr>
2022-11-08 10:51:49 +0100

    Carte.java

--- ProjetTrajets/src/net/lecnam/trajets/Carte.java
+++ ProjetTrajets/src/net/lecnam/trajets/Carte.java
@@ -1,5 +1,27 @@
 package net.lecnam.trajets;
 
-public class Carte {
+import java.util.ArrayList;
 
+public class Carte {
+	private ArrayList<Trajet> trajets;
+	
+	public Carte(ArrayList<Trajet> trajets) {
+		this.trajets = trajets;
+	}
+	
+	public void construireCarte() {
+		
+	}
+	
+	public ArrayList<Trajet> getTrajets(){
+		return this.trajets;
+	}
+	
+	public void setTrajets(ArrayList<Trajet> trajets) {
+		this.trajets = trajets;
+	}
+	
+	public void addTrajet(Trajet t) {
+		this.trajets.add(t);
+	}
 }
