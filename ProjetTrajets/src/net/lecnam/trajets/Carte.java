3d197c19e34adc0adf676aafb0c79415e050234c
Christophe LEDUC <christophe.leduc@etudiant.univ-reims.fr>
2022-11-08 11:14:47 +0100

    Carte.java + carte.txt

--- ProjetTrajets/src/net/lecnam/trajets/Carte.java
+++ ProjetTrajets/src/net/lecnam/trajets/Carte.java
@@ -1,16 +1,24 @@
 package net.lecnam.trajets;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Carte {
 	private ArrayList<Trajet> trajets;
 	
-	public Carte(ArrayList<Trajet> trajets) {
-		this.trajets = trajets;
+	public Carte() {
+		this.trajets = new ArrayList<Trajet>();
 	}
 	
-	public void construireCarte() {
-		
+	public void construireCarte(String fichier) throws FileNotFoundException {
+		File myFile = new File(fichier);
+		Scanner s = new Scanner(myFile);
+		s.useDelimiter(" ");
+		while(s.hasNext()) {
+			
+		}
 	}
 	
 	public ArrayList<Trajet> getTrajets(){
