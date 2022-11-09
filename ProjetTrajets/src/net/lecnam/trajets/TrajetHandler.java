024ce3b7075d8997fb00952fb1be5724aa2e96b3
Lilierd <67808827+Lilierd@users.noreply.github.com>
2022-11-09 08:53:29 +0100

    Ajout du handler

--- ProjetTrajets/src/net/lecnam/trajets/TrajetHandler.java
+++ ProjetTrajets/src/net/lecnam/trajets/TrajetHandler.java
@@ -1,5 +1,53 @@
 package net.lecnam.trajets;
 
-public class TrajetHandler {
+import java.util.ArrayList;
 
+public class TrajetHandler {
+	Carte carte;
+	ArrayList<Trajet> trajetFinal;
+	
+	public TrajetHandler(Carte map) {
+		this.carte = map;
+	}
+	
+	private Trajet getShorterRoute(Ville villeDepart) {
+		ArrayList<Trajet> trajets = new ArrayList<Trajet>();
+		for(Trajet t : this.carte.getTrajets()) {
+			if(t.getVille1().getNom() == villeDepart.getNom()
+					|| t.getVille2().getNom() == villeDepart.getNom())
+				trajets.add(t);
+		}
+		Trajet shorterTrajet = this.carte.getTrajets().get(0);
+		int dist = shorterTrajet.getLongueur();
+		for(Trajet t : trajets) {
+			if(dist > t.getLongueur()) {
+				dist = t.getLongueur();
+				shorterTrajet = t;
+			}
+		}
+		return shorterTrajet;
+	}
+	
+	public Carte getCarte() {
+		return carte;
+	}
+	
+	public void setCarte(Carte carte) {
+		this.carte = carte;
+	}
+
+	public ArrayList<Trajet> getTrajetFinal(Ville villeDepart, Ville villeArrivee) {
+		Ville currentCity = villeDepart;
+		
+		while(currentCity.getNom() != villeArrivee.getNom()) {
+			Trajet t = this.getShorterRoute(currentCity);
+			trajetFinal.add(t);
+			if(currentCity.getNom() == t.getVille1().getNom())
+				currentCity = t.getVille2();
+			else
+				currentCity = t.getVille1();
+		}
+		
+		return trajetFinal;
+	}
 }
