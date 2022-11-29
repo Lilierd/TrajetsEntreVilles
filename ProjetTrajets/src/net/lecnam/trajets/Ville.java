a5d042c5223e82b6eb4c3dd6eec579578aa97093
Lilierd <67808827+Lilierd@users.noreply.github.com>
2022-11-29 11:58:20 +0100

    Update Ville.java
    Ajout de l'attribut "distance"

--- ProjetTrajets/src/net/lecnam/trajets/Ville.java
+++ ProjetTrajets/src/net/lecnam/trajets/Ville.java
@@ -4,10 +4,12 @@ package net.lecnam.trajets;
 public class Ville {
 	private String nom;
 	private boolean visitee;
+	private int distance;
 	
 	public Ville(String name) {
 		this.nom = name;
 		this.visitee = false;
+		this.distance = Integer.MAX_VALUE;
 	}
 
 	public String getNom() {
@@ -25,6 +27,13 @@ public class Ville {
 	public void setVisitee(boolean isVisited) {
 		this.visitee = visitee;
 	}
-	
+
+	public int getDistance() {
+		return this.distance;
+	}
+
+	public void setDistance(int distance) {
+		this.distance = distance;
+	}
 	
 }
